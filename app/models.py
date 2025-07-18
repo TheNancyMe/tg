f60e6bb9473408cc3f91c43337902add5ddcb636
TheNancyMe <155481350+TheNancyMe@users.noreply.github.com>
2025-07-18 14:58:58 +0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -3,9 +3,10 @@ from .database import Base
 from sqlalchemy import BigInteger
 
 class Note(Base):
-    __tablename__ = "notes"  # имя таблицы в базе данных
+    __tablename__ = "notes"  # <= обязательно! имя таблицы
 
-    id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(BigInteger, index=True)
-    content = Column(String, nullable=False)
+    id = Column(Integer, primary_key=True, index=True)  # <= Primary Key есть
+    user_id = Column(BigInteger, index=True, nullable=False)
+    title = Column(String(255), nullable=False)         # Заголовок
+    description = Column(String, nullable=True)         # Описание может быть пустым
     created_at = Column(DateTime(timezone=True), server_default=func.now())
