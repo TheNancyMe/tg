61d504b004427bbbb8f8a07a06f166fcca9813da
TheNancyMe <155481350+TheNancyMe@users.noreply.github.com>
2025-08-05 12:23:14 +0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -2,18 +2,12 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-# Получаем строку подключения из переменной окружения
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-# Создаём асинхронный движок SQLAlchemy
 engine = create_async_engine(DATABASE_URL, echo=True)
-
-# Создаём сессию
-SessionLocal = sessionmaker(
-    bind=engine,
-    class_=AsyncSession,
-    expire_on_commit=False
-)
-
-
+AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 Base = declarative_base()
+
+async def get_session() -> AsyncSession:
+    async with AsyncSessionLocal() as session:
+        yield session
